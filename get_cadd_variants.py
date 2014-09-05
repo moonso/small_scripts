31ecde3cf23299addc335012e5da5691c72e21ae
Mans Magnusson <monsunas@gmail.com>
2014-09-05 14:56:44 +0200

    Added get_cadd_variants.py

--- get_cadd_variants.py
+++ get_cadd_variants.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-get_rare_variants.py
+get_cadd_variants.py
 
-Remove all variants where MAF > choosen frequency.
+Remove all variants where CADD score < choosen treshold.
 
 Created by Måns Magnusson on 2014-04-30.
 Copyright (c) 2014 __MoonsoInc__. All rights reserved.
@@ -30,18 +30,18 @@ if sys.version_info < (3,0):
                     type=click.Path(exists=True),
                     metavar='<vcf_file>'
 )
-@click.option('--rare', '-rare',
-                    default=0.05,
-                    help="Set the treshold for rare variants. Default 0.05"
+@click.option('--cadd_treshold', '-c',
+                    default=15,
+                    help="Set the treshold for cadd score. Default 15"
 )
 @click.option('-kw' ,'--keyword', 
-                    default='AF',
+                    default='CADD',
                     help="""Set the keyword for which info field that should be considered.
-                            Default is 'AF'"""
+                            Default is 'CADD'"""
 )
-def get_rare(variant_file, rare, keyword):
+def get_cadd(variant_file, cadd_treshold, keyword):
     """Print only the rare variants to a new vcf"""
-    
+    cadd_treshold = float(cadd_treshold)
     if variant_file == '-':
         variant_parser = vcf_parser.VCFParser(fsock = sys.stdin)
     else:
@@ -50,10 +50,9 @@ def get_rare(variant_file, rare, keyword):
     for line in variant_parser.metadata.print_header():
         print(line)
     for variant in variant_parser:
-        interesting = True
-        maf = min([float(allele) for allele in variant['info_dict'].get(keyword,'0').split(',')])
-        if maf < rare:
+        cadd_score = float(variant['info_dict'].get(keyword,'0'))
+        if cadd_score >= cadd_treshold:
             print('\t'.join([variant[entry] for entry in variant_parser.metadata.header]))
     
 if __name__ == '__main__':
-    get_rare()
+    get_cadd()
